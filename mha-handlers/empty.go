62f5ed77c694fbd022a6e6a9196ae8a2ebdee479
lindan <lindan@bsgchina.com>
2015-01-23 17:54:04 +0800

    add mha-handlers

---
+++ mha-handlers/empty.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+//	"bytes"
+	"github.com/astaxie/beego"
+	"net/http"
+	"net/url"
+)
+
+func Empty() {
+	client := new(http.Client)
+	emptykvurl := &url.URL{
+		Scheme: "http",
+		Host:   "192.168.2.71:8500",
+		Path:   "/v1/kv/service/mysql-1/leader",
+	}
+	//emptykv := ""
+	emptyreg, err := http.NewRequest("PUT", emptykvurl.String(), nil)
+	if err != nil {
+		beego.Error("Update service/mysql-1/leader request failed!", err)
+		return
+	}
+	beego.Info("Update service/mysql-1/leader request success!")
+	emptyreg.Header.Set("Content-Type", "application/json")
+	emptyresg, err := client.Do(emptyreg)
+	if emptyresg.StatusCode != 200 {
+		beego.Error("Update service/mysql-1/leader failed!", err)
+		return
+	}
+	beego.Info("Update service/mysql-1/leader success!")
+}
---
+++ mha-handlers/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	beego.SetLogger("file", `{"filename":"/home/lindan/lindan/github/consul/mha-handlers/logs/handlers.log"}`)
+	beego.SetLogFuncCall(true)
+}
+
+func main() {
+	defer beego.BeeLogger.Close()
+	Empty()
+}
